f6c5fc17697254dc2ba972d956092763d985bdc8
Mahoney96 <cmahoney966@gmail.com>
2020-08-14 02:15:09 -0700

    dependencies installed

---
+++ src/Components/House/House.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const House = () => {
+    return <div>
+        This is a House Component!!
+    </div>
+}
+
+export default House;
---
+++ src/Components/Wizard/Wizard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Wizard = () => {
+    return <div>
+        This is a wizard Component!!
+    </div>
+}
+
+export default Wizard;
